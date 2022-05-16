2c155f3ef5ee416e42452a91664626dde88283c5
eli.pi.oldi11@gmail.com <prisma@prismanode.local>
2022-05-16 14:47:42 +0200

    Update permissions file, remove docker binds

--- SETUP/configure.sh
+++ SETUP/configure.sh
@@ -34,9 +34,9 @@ ELLE_PASS="${FILES}/passwd.txt"
                 docker exec -it prisma-orma chmod -R 770 /var/www/html/tmp-media
                 docker exec -it prisma-orma chown -R www-data:www-data /var/www/html/info-media
                 docker exec -it prisma-orma chmod -R 770 /var/www/html/info-media
-                chown -R root:www-data /etc/openvpn
+                chown -R root:prisma /etc/openvpn
                 chmod -R 775 /etc/openvpn
-                chown -R root:www-data /etc/prometheus
+                chown -R root:prisma /etc/prometheus
                 chmod -R 775 /etc/prometheus
                 
                 echo "Ended"
