cd3d41995252ed1700aa223734f560fa4655fdfd
Andrea <andrea.novati@n-3.it>
2024-06-24 15:29:42 +0200

    spolitted logi into 2 files

--- SETUP/clean_disk.sh
+++ SETUP/clean_disk.sh
@@ -1,67 +1,46 @@
 #!/bin/bash
-#/home/prisma/clean_disk.sh /prismadata/ITTO12/ 360 80 
-echo "Starting PRISMA clean disk"
+#/home/user/clean_disk.sh /userdata/ITTO12/ 360 80 
 
-prismadata_path=$1
+data_path=$1
 start_days=$2
 percentage=$3
-
 du=0
 
-function compute_du(){
+function compute_du()
+{
  disk_usage_command="df -h | grep /dev/nvme0n1p2 | awk -F% '{print \$1}'"
  #echo $disk_usage_command
  df_output=`eval $disk_usage_command`
  du=${df_output: -3}
 }
 
+echo "Starting clean_disk"
+
 if [[ -z $percentage ]]; then
   percentage=80
 fi
-
 
 compute_du
 
 echo "Disk usage: $du%"
 
 if [[ $du -gt $percentage ]]; then
- echo "Disk Usage $du% ecceded $percentage%, removing files of $start_days days ago."
+ echo "Disk Usage $du% ecceded $percentage%, removing files older than $start_days days ago..."
 
  if [[ -z $start_days ]]; then
   start_days=360
  fi
- if [[ -z  $prismadata_path ]]; then
+ if [[ -z  $data_path ]]; then
 	echo "usage: $0 /prismadata/<STATION CODE>/ <Start days> <Percentage> eg. /prismadata/ITLO06/ 360 80"
 	echo "start days: need to be at least 60"
 	exit;
  fi
 
- for (( days=$start_days; days>=60; days-- ))
- do
-  current_date=$(date +%s)
-  days_ago=$(($current_date - $days * 86400))
-  find_command="find $prismadata_path -type f \( ! -name 'default.bmp' \) -exec sh -c '
-    echo \"Processing file: \$1\"
-    file_birth_time=\$(stat -c %W \"\$1\")
-    if [ \"\$file_birth_time\" -lt \"$days_ago\" ]; then
-       echo \"\$1 is older than $days days\"
-    fi
-' {} \;"
+ find_command="find $data_path -type f \( ! -name 'default.bmp' \) -exec sh -c './process_file.sh' {} $start_days $percentage \;"
 
+ echo $find_command
+ #eval $find_command 
 
-  echo $find_command
-  #eval $find_command 
-
-  compute_du
-
-  if [[ $du -gt $percentage ]]; then
-   echo "Disk Usage $du% exceded $percentage, removing files of $days days ago."
-  else
-   echo "Finished removing files" 
-   break
-  fi
-
- done
 else
-echo "Nothing to do"
+ echo "Nothing to do"
 fi
---
+++ SETUP/process_file.sh
@@ -0,0 +1,40 @@
+ #!/bin/bash
+ du=0
+ 
+ file_path = $1
+ start_days = $2
+ percentage = $3
+
+ current_date=$(date +%s)
+ 
+ function compute_du()
+ {
+  disk_usage_command="df -h | grep /dev/nvme0n1p2 | awk -F% '{print \$1}'"
+  #echo $disk_usage_command
+  df_output=`eval $disk_usage_command`
+  du=${df_output: -3}
+ }
+
+ echo "Start Days: $1"
+ echo "Processing file: $2"
+ echo "Disk usage: $du%"
+
+
+ for (( days=$start_days; days>=60; days-- ))
+ do
+  days_ago=$(($current_date - $days * 86400))
+  file_birth_time=$(stat -c %W "$file_path")
+  
+  if [ "$file_birth_time" -lt "$days_ago" ]; then
+	echo "$file_path is older than $days days"
+  fi
+    
+  compute_du
+
+  if [[ $du -gt $percentage ]]; then
+   echo "Disk Usage $du% exceded $percentage%, removing files older than $days days ago."
+  else
+   echo "Finished removing files" 
+   break
+  fi
+done
